f65a86030ce9453fd296d2c6311364ddff1d4974
vishalkokane264 <vishalkokane264@gmail.com>
2024-01-31 14:02:48 +0530

    new swiggy added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-
+import swiggyIcon from "./../assets/swiggy-logo.svg";
+import notificationIcon from "./../assets/notification.png";
 const HeaderPage = () => {
   return (
     <div className="header-component border flex-row justify-between items-center gap-10">
       <img
-        src="/next-logo.png"
+        src={swiggyIcon.src}
         alt="sample image"
-        height={"70px"}
+        height={"100px"}
         width={"100px"}
       />
       <div className="search-box">{/* <input type="search" /> */}</div>
       <div className="notification">
         <img
-          src="/notification.png"
+          src={notificationIcon.src}
           height={"30px"}
           width={"30px"}
           alt="notification"
